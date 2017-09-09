358370103736e8fb115b8595ecad07277f2efa0c
Jonathan Somer <somer@mail.tau.ac.il>
2017-09-09 12:39:30 +0300

    testScript

--- Tests/testScript.sh
+++ Tests/testScript.sh
@@ -4,8 +4,7 @@ do
 	res=$(./../cmake-build-debug/ChessProject.exe < test_"$i".in > test_"$i".res)
 	
 	#res=$(./vault my_repository.vlt add folder1/folder2/data_filter"$i".c)
-	res=$(diff test_"$i".res test_"$i".out)
-	echo $res
+	res=$(diff test_"$i".res test_"$i".out)	
 	if [ "$res" != "" ]; then
 		echo FAIL:  test number $i
 	else 
