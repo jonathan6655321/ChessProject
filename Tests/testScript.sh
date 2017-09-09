c8dc6e47593a6ed316c4b74bba32da6f2ad775da
Jonathan Somer <somer@mail.tau.ac.il>
2017-09-09 12:34:47 +0300

    script works

--- Tests/testScript.sh
+++ Tests/testScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for i in `seq 1 1`
-do
-	
+do	
 	res=$(./../cmake-build-debug/ChessProject.exe < test_"$i".in > test_"$i".res)
 	
 	#res=$(./vault my_repository.vlt add folder1/folder2/data_filter"$i".c)
 	res=$(diff test_"$i".res test_"$i".out)
+	echo $res
 	if [ "$res" != "" ]; then
 		echo FAIL:  test number $i
 	else 
