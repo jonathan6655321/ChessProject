f8c58e85149e0b67178793e2e2199f2f03cde06c
GameXtra <Kessido@Gmail.com>
2017-09-09 19:07:02 +0300

    minimax file

---
+++ MiniMaxAlgorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by kessi on 09/09/2017.
+//
+
+#ifndef CHESSPROJECT_MINIMAXALGORITHM_H
+#define CHESSPROJECT_MINIMAXALGORITHM_H
+
+int alpha, beta;
+void
+int getBestActionMinMaxPruning();
+
+int getBestActionMinMaxPruning();
+
+#endif //CHESSPROJECT_MINIMAXALGORITHM_H
