e356fde45e196cfb28c6b3632426b707f1b14300
GameXtra <Kessido@Gmail.com>
2017-09-30 19:33:19 +0300

    return the gui loop to the code

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "CommandLineGame.h"
-// TODO:#include "Gui/mainGui.h"
+#include "Gui/mainGui.h"
 
 int main(int argc, char **argv) {
 	if (argc == 2 && strcmp("-g", argv[1]) == 0) {
-		return 0;//TODO:MainGuiLoop();
+		return MainGuiLoop();
 	} else {
 		return commandLineGameLoop();
 	}
