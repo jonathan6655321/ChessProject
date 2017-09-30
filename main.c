651a2ca5d374b902d8d31f70a1aecc80b9d5ed06
Jonathan Somer <jonathan.somer@gmail.com>
2017-09-30 19:48:30 +0300

    check if this compiles

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
-//////
-////// Created by Jonathan on 7/18/2017.
-//////
-//#include <stdlib.h>
-//#include <string.h>
-//#include "CommandLineGame.h"
-//#include "Gui/mainGui.h"
-//
-//int main(int argc, char **argv) {
-//	if (argc == 2 && strcmp("-g", argv[1]) == 0) {
-//		return MainGuiLoop();
-//	} else {
-//		return commandLineGameLoop();
-//	}
-//}
+////
+//// Created by Jonathan on 7/18/2017.
+////
+#include <stdlib.h>
+#include <string.h>
+#include "CommandLineGame.h"
+#include "Gui/mainGui.h"
+
+int main(int argc, char **argv) {
+	if (argc == 2 && strcmp("-g", argv[1]) == 0) {
+		return MainGuiLoop();
+	} else {
+		return commandLineGameLoop();
+	}
+}
